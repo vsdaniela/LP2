4ffa8e55b922ea073ba0c56abb3ea7d6e69ecb51
vsdaniela <42683715+vsdaniela@users.noreply.github.com>
2019-04-25 14:08:32 -0500

    Add files via upload

---
+++ class matrix/M_test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Matrix.h"
+using namespace std;
+int main()
+{
+        Matrix <int> x;
+        /*int a_[2][3]={{1,2,-3},{4,0,-2}};
+        int b[3][1]={{3},{2},{-1}};
+        int c[2][1];
+        for(int i=0;i<2;i++){
+            for(int j=0;j<1;j++){
+                    c[i][j]=0;
+                    for(int k=0;k<3;k++){
+                        c[i][j]=c[i][j]+(a_[i][k]*b[k][j]);
+                    }
+                    cout<<c[i][j]<<endl;
+            }
+        }*/
+}
